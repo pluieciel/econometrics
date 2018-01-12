0f668bb08f1e90cec0e62658e7d12398f34b837e
pluieciel <yorke5@qq.com>
2018-01-12 23:40:53 +0100

    Update Class 1 OLS.py

--- Class 1 OLS.py
+++ Class 1 OLS.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
-#import numpy as np
 import statsmodels.api as sm
 import csv
+
 with open(r'D:\france life\2017 T2\metric\costsalary.csv','r') as csvf:
-    a=csv.reader(csvf)
-    x=[row[0].split(';') for row in a][1:]
-    
+    x=[row[0].split(';') for row in csv.reader(csvf)][1:]
 x,y=zip(*x)
 x,y=list(map(int,x)),list(map(int,y))
 X = sm.add_constant(x)
 results = sm.OLS(y, X).fit()
 print(results.summary())
 plt.plot(x,y,'bo',x,results.fittedvalues,'r')
-
 
 '''
 R:
@@ -25,5 +22,4 @@ y=a$Salary
 r=lm(y~x)
 summary(r)
 plot(x,y,'o')
-
 '''
