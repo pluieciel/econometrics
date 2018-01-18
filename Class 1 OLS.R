751472e1666cf51dde67c30a88a0734df91e6c97
pluieciel <yorke5@qq.com>
2018-01-18 21:47:43 +0100

    Update Class 1 OLS.R

--- Class 1 OLS.R
+++ Class 1 OLS.R
@@ -1,4 +1,4 @@
-a = read.csv("D:/france life/2017 T2/metric/costsalary.csv", header = TRUE, sep=";")
+a = read.csv("https://raw.githubusercontent.com/pluieciel/econometrics/master/costsalary.csv", header = TRUE, sep=";")
 x=a$Costs
 y=a$Salary
 r=lm(y~x)
