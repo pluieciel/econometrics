d92b6a0f47a176e7550857d99bb9e3eac1106c8b
pluieciel <yorke5@qq.com>
2018-02-02 22:00:09 +0100

    Update 1-2. costsalary_firstprogram.py

--- 1-2. costsalary_firstprogram.py
+++ 1-2. costsalary_firstprogram.py
@@ -5,7 +5,7 @@ import statsmodels.api as sm
 from io import StringIO  
 import urllib.request
 
-data = urllib.request.urlopen('https://raw.githubusercontent.com/pluieciel/econometrics/master/costsalary.csv').read().decode('ascii', 'ignore')
+data = urllib.request.urlopen('https://raw.githubusercontent.com/pluieciel/econometrics/master/data/costsalary.csv').read().decode('ascii', 'ignore')
 my = np.loadtxt(StringIO(data),delimiter=";",skiprows=1)
 # my = np.loadtxt(open(r'D:\france life\2017 T2\metric\costsalary.csv',"rb"),delimiter=";",skiprows=1)
 x, y = zip(*my)
