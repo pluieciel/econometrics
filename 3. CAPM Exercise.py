eabdaf4848e3cc3eb11e4b0a6ca385fad257988e
pluieciel <yorke5@qq.com>
2018-02-04 16:54:16 +0100

    Update 3. CAPM Exercise.py

--- 3. CAPM Exercise.py
+++ 3. CAPM Exercise.py
@@ -4,17 +4,26 @@ import numpy as np
 import statsmodels.api as sm
 from io import StringIO  
 import urllib.request
-import scipy
+import scipy, csv
 
+#####load datas:
+##from web:
 data = urllib.request.urlopen('https://raw.githubusercontent.com/pluieciel/econometrics/master/data/CAPM%20Exercise.csv').read().decode('ascii', 'ignore')
 my = np.loadtxt(StringIO(data),delimiter=",",skiprows=1)
 
 ##if from local path:
-# my = np.loadtxt(open(r'D:\Users\JVKR\Dropbox\VRP\BD_VRP\HighFreqData\AAPL1min_NASDAQ.csv',"rb"),delimiter=";",skiprows=1)
+#with open(r'C:\Users\ywluyao11\Downloads\CAPM Exercise.csv','r') as csvf:
+#    a=csv.reader(csvf)
+#    my=[row for row in a][1:]
 
 y, x = zip(*my) #x:sp500, y:AMZN
+
+##if from local path:
+#y,x=list(map(float,y)),list(map(float,x))
+
 X = sm.add_constant(x) #X is a matrix with first col of '1' & second col of x
 
+#####regression:
 regression = sm.OLS(y,X).fit()
 print(regression.summary())
 plt.plot(x,y,'.',x,regression.fittedvalues,'r')
