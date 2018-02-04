c1fe9a9c54b22ee8a41416cc8e02b112321d9f8b
pluieciel <yorke5@qq.com>
2018-02-04 17:00:39 +0100

    Update 1-2. costsalary_firstprogram.py

--- 1-2. costsalary_firstprogram.py
+++ 1-2. costsalary_firstprogram.py
@@ -15,10 +15,12 @@ print(results.summary())
 #with plt.xkcd():    #XKCD-style sketch plots ;-)
 plt.plot(x,y,'bo',x,results.fittedvalues,'r')
 
-#method 2:do it manually for alpha and beta θ=(X.T*X).I*X.T*y
+#####method 2:
+#normal equition: alpha and beta θ=(X.T*X).I*X.T*y
 A=(np.mat(X).T*np.mat(X)).I*np.mat(X).T*np.mat(y).T   
 a,b=A[0,0],A[1,0]
 
-#method 3:beta=Cov/Var
+#####method 3:
+#beta=Cov/Var
 beta= np.cov(x,y)[1][0]/np.cov(x,y)[0][0]
 alpha=np.mean(y)-beta*np.mean(x)
