bd3217f9d1bb658c6ba3d76deaa60e0e42d7c473
pluieciel <yorke5@qq.com>
2018-03-04 17:17:34 +0100

    Update 6. salary_experience_Chowtest.py

--- 6. salary_experience_Chowtest.py
+++ 6. salary_experience_Chowtest.py
@@ -15,27 +15,31 @@ salary=salary_experience['vy']
 experience=salary_experience['vx']
 education=salary_experience['vxz']
 
-plt.plot(experience,salary,'k.')
-
 df=pd.DataFrame({
         'salary':salary,
         'experience':experience,
         'education':education
         })
+
+#regression:
 model1=ols('salary~experience',df).fit()
 print(model1.summary())
+#plot:
+plt.plot(experience,salary,'k.')
 plt.plot(experience,model1.fittedvalues,'b')
 plt.show()
+
+###subsets:
 #points
 plt.plot(experience[education==1],salary[education==1],'g.')
 plt.plot(experience[education==0],salary[education==0],'r.')
 
 eduexp=education*experience
-df['eduexp']=eduexp
+df['eduexp']=eduexp  #update DataFrame
 model2=ols('salary~experience+education+eduexp',df).fit()
 
 #regression lines
-a,b,c,d=model2.params
+a,b,c,d=model2.params  #beta0,beta1,beta2,beta3
 plt.plot(experience,a+b*experience,'r')
 plt.plot(experience,(a+c)+(b+d)*experience,'g')
 plt.show()
@@ -54,12 +58,13 @@ Ftest=((ssrr-ssru)/nrestr)/(ssru/(len(salary)-len(model2.params)))
 print('Ftest=',Ftest,'\n'*3)
 
 #critical value
-#qf(0.95,nrestr,length(salary)-length(model2$coefficients))
+#R: qf(0.95,nrestr,length(salary)-length(model2$coefficients))
 print(scipy.stats.f.ppf(0.95,nrestr,len(salary)-len(model2.params)),'\n'*3)
-#1-pf(Ftest,nrestr,length(salary)-length(model2$coefficients))
+#R: 1-pf(Ftest,nrestr,length(salary)-length(model2$coefficients))
 print(1-scipy.stats.f.cdf(Ftest,nrestr,len(salary)-len(model2.params)),'\n'*3)
-#anova(model2,model1)
+#R: anova(model2,model1)
 print(statsmodels.stats.anova.anova_lm(model1,model2))
+#Attention:
 print('\033[1;31;43m','Attention: the input order impacts the F value. input Restricted Model first','\033[0m','\n'*3)
 
 mlow=salary_experience[salary_experience['vxz']==0]
