083630ceba1a33cf37b84e24e4d8e1d7b8b7ce18
pluieciel <yorke5@qq.com>
2018-03-19 11:35:49 +0100

    Update 8. gdpfrance.py

--- 8. gdpfrance.py
+++ 8. gdpfrance.py
@@ -7,18 +7,18 @@ from scipy.optimize import minimize
 from statsmodels.graphics.tsaplots import plot_acf
 ###load data:
 ##from web:
-#data = StringIO(urllib.request.urlopen('https://raw.githubusercontent.com/pluieciel/econometrics/master/data/France%20GDP.csv').read().decode('ascii', 'ignore'))
-#AER_App_data=pd.read_table(data,sep=',')
+data = StringIO(urllib.request.urlopen('https://raw.githubusercontent.com/pluieciel/econometrics/master/data/France%20GDP.csv').read().decode('ascii', 'ignore'))
+France_GDP=pd.read_table(data,sep=',')
 ##from local:
-France_GDP=pd.read_table(r'D:\france life\2017 T2\econometrics\France GDP.csv',sep=',')
+#France_GDP=pd.read_table(r'D:\france life\2017 T2\econometrics\France GDP.csv',sep=',')
 
 ######
 gdp=France_GDP["Value"]
 plt.plot(gdp,'.')
+plt.show()
 plt.plot(gdp)
-
+plt.show()
 rng = pd.period_range('1961Q1','2017Q4', freq='Q')
 ts=pd.Series(np.array(gdp),index=rng)
 ts.plot()
-
 plot_acf(gdp)
